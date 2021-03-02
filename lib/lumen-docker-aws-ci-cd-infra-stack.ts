7dcde37dd35d7232935d88ca1b018889c3666642
Chris Yang <kimisme9386@gmail.com>
2021-03-02 22:52:06 +0800

    Add slack notification for pipeline

--- lib/lumen-docker-aws-ci-cd-infra-stack.ts
+++ lib/lumen-docker-aws-ci-cd-infra-stack.ts
@@ -2,8 +2,12 @@ import * as codebuild from '@aws-cdk/aws-codebuild';
 import * as codepipeline from '@aws-cdk/aws-codepipeline';
 import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
 import { CodeBuildActionType } from '@aws-cdk/aws-codepipeline-actions';
+import * as targets from '@aws-cdk/aws-events-targets';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as ssm from '@aws-cdk/aws-ssm';
 import * as cdk from '@aws-cdk/core';
 import { Aws, Tags } from '@aws-cdk/core';
+import * as path from 'path';
 
 export class LumenDockerAwsCiCdInfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -64,11 +68,56 @@ export class LumenDockerAwsCiCdInfraStack extends cdk.Stack {
         }),
       ],
     });
+
+    const slackWebhookPath = ssm.StringParameter.valueFromLookup(
+      this,
+      '/codepipeline/notification/slack-webhook-path'
+    );
+    const targetLambda = this.createEventLambdaFunction(
+      `https://hooks.slack.com/services${slackWebhookPath}`
+    );
+
+    pipeline.onStateChange('CodePipelineStateChange', {
+      eventPattern: {
+        source: ['aws.codepipeline'],
+        detailType: ['CodePipeline Pipeline Execution State Change'],
+        detail: {
+          state: ['STARTED', 'CANCELED', 'FAILED'],
+        },
+      },
+      target: new targets.LambdaFunction(targetLambda),
+    });
+
+    pipeline.onStateChange('CodePipelineActionStateChange', {
+      eventPattern: {
+        source: ['aws.codepipeline'],
+        detailType: ['CodePipeline Action Execution State Change'],
+        detail: {
+          state: ['STARTED', 'CANCELED', 'FAILED'],
+        },
+      },
+      target: new targets.LambdaFunction(targetLambda),
+    });
   }
 
   tagResource(scope: cdk.Construct): void {
     // ref: https://github.com/aws/aws-cdk/issues/4134
     Tags.of(scope).add('CDK-CfnStackId', Aws.STACK_ID);
     Tags.of(scope).add('CDK-CfnStackName', Aws.STACK_NAME);
+  }
+
+  createEventLambdaFunction(slackWebhookURL: string): lambda.Function {
+    const lambdaFunc = new lambda.Function(this, 'CodepipelineEventLambda', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset(path.join(__dirname, '../lambda')),
+      handler: 'codepipelineEventLambda.handler',
+      environment: {
+        SLACK_WEBHOOK_URL: slackWebhookURL,
+      },
+    });
+
+    this.tagResource(lambdaFunc);
+
+    return lambdaFunc;
   }
 }
